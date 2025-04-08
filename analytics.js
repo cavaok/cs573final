fd67726b3cc2be2f97c9f3cd2ba6914b89e71563
Olivia Cava <oliviacava@Olivias-MacBook-Pro-1769.local>
2025-04-08 13:09:42 -0400

    update styling

--- analytics.js
+++
@@ -1,9 +0,0 @@
-// This file will contain code for the analytics page visualizations
-function initAnalyticsVisualization() {
-    // Your visualization code here
-    const spec = {
-        // Your Vega-Lite specification for info visualizations
-    };
-    
-    vegaEmbed('#analytics-visualization', spec);
-}
--- exploration.js
+++
@@ -1,9 +0,0 @@
-// This file will contain code for the exploration page visualizations
-function initExplorationVisualizations() {
-    // Your visualization code here
-    const spec = {
-        // Your Vega-Lite specification for info visualizations
-    };
-    
-    vegaEmbed('#exploration-visualization', spec);
-}
